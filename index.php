d1e45e14a5303d413b8e835fcafbf420e12c2afa
naoki85 <naoki.yoneyama.85@gmail.com>
2021-10-31 17:31:50 +0900

    made index.php an error

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <?php
-define('NUM', 10);
-
-$first = 0;
-$second = 0;
-for ($i = 1; $i <= NUM; $i++) {
-  if ($i === 1) {
-    echo "1\n";
-    $first = 1;
-    continue;
-  } elseif ($i === 2) {
-    echo "1\n";
-    $second = 1;
-    continue;
+class Fibonacci
+{
+  function print($number) {
+    $hogehoge = null;
+    $first = 0;
+    $second = 0;
+    for ($i = 1; $i <= $number; $i++) {
+      if ($i === 1) {
+        echo "1\n";
+        $first = 1;
+        continue;
+      } elseif ($i === 2) {
+        echo "1\n";
+        $second = 1;
+        continue;
+      }
+    
+      $now = $first + $second;
+      echo $now."\n";
+      $first = $second;
+      $second = $now;
+    }
   }
-
-  $now = $first + $second;
-  echo $now."\n";
-  $first = $second;
-  $second = $now;
 }
+
+(new Fibonacci())->print(10);
